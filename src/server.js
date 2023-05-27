bf0f9d1adfea95955c0c9d12be50d3609d5e316c
MHMMDDzaki <dzakibs@gmail.com>
2023-05-27 14:06:11 +0700

    added add data and read data

--- src/server.js
+++ src/server.js
@@ -18,14 +18,49 @@ const Firestore = require('@google-cloud/firestore');
 
 const db = new Firestore({
   projectId: 'capstone-project-c23pc717',
-  keyFilename: '../keyfile.json',
+  keyFilename: 'keyfile.json',
 });
 
 // read data
-const cityRef = db.collection('users').doc('GK6dXs5Qry0Jco74iCbV');
-const doc = cityRef.get();
-if (!doc.exists) {
-  console.log('No such document!');
-} else {
-  console.log('Document data:', doc.data());
+async function readData(databases) {
+  const tester = await databases.collection('users').get();
+  tester.forEach((doc) => {
+    console.log(doc.id, '=>', doc.data());
+  });
 }
+
+// readData(db)
+
+// add data
+async function addData(database) {
+  const docRef = database.collection('users').doc(makeid(8));
+  await docRef.set({
+    username: 'Ada',
+    password: 'Lovelace',
+    userID: '43A34'
+  });
+}
+
+addData(db)
+
+function makeid(length) {
+  let result = '';
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const charactersLength = characters.length;
+  let counter = 0;
+  while (counter < length) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    counter += 1;
+  }
+  return result;
+}
+
+// console.log(makeid(8));
+
+// tester.get().then(doc => {
+//   const data = doc.data();
+//   console.log(data)
+// });
+
+
+
